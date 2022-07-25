35b688702c0d7b67a69a2d6ad4f5d38522161186
matiss <matissjurevics@gmail.com>
2022-07-25 21:12:47 +0100

    added a collections page

---
+++ src/routes/app/Collections/index.svelte
@@ -0,0 +1,15 @@
+<script>
+    let collections = [
+        "test",
+        "boop"
+    ]
+</script>
+
+<main>
+    <h1>Collections</h1>
+    <ul>
+        {#each collections as collection}
+             <li>{collection}</li>
+        {/each}
+    </ul>
+</main>
